8e68317afdcc31f01bd412af37bf24ca8500322e
Areej Alkhoder <areejalkhoder@gmail.com>
2025-02-27 08:09:15 +0400

    add auth

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <p>Next.js</p>
+            <ul>
+                <Link href>
+
+                </Link>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
